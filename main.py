b6a9612ffd118558520dc4aed8e47f914b8ab877
Rushikesh Randive <rushirandive09@gmail.com>
2026-03-06 17:37:59 +0530

    Add Vercel frontend URL to CORS

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://rayeva-ai-frontend.vercel.app",  # Your Vercel URL
+        "https://rayeva-ai-modules.vercel.app/",  # Your Vercel URL
         "https://*.vercel.app",  # All Vercel preview URLs
     ],
     allow_credentials=True,
